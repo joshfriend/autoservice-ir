b988ec9e590d0d044667e45c155b97307b0d88aa
Josh Friend <josh@fueledbycaffeine.com>
2026-01-31 09:25:59 -0500

    update testkit

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,7 +5,7 @@ pluginManagement {
     id("com.gradle.plugin-publish") version "1.3.1"
     id("org.jetbrains.kotlin.jvm") version "2.3.0"
     id("com.autonomousapps.build-health") version "3.5.1"
-    id("com.autonomousapps.testkit") version "0.13"
+    id("com.autonomousapps.testkit") version "0.17"
     id("org.jetbrains.kotlinx.binary-compatibility-validator") version "0.17.0"
     id("com.github.gmazzo.buildconfig") version "6.0.7"
     id("com.vanniktech.maven.publish") version "0.36.0"
